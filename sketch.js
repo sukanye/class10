1097041db06ef2500ba485e9767452e55f60ee1a
sukanye <ray.suk@husky.neu.edu>
2016-03-28 16:19:25 -0400

    :) things now show up
    @FranDias

--- sketch.js
+++ sketch.js
@@ -1,29 +1,8 @@
+var hit=false;
 var dataLength = data.length;
-var dataArr = []; //empty array 
-var dataArrLength; //creating this variable, equal to dataArr.length
-var dataValueText = "Hover Over To See Values"
-
-//Constructor called aData 
-var aData = function(localHeight){
-  this.height = localHeight; 
-  this.width = 10;
-  this.renderRect= function(x,y){
-    fill("#bada55");
-    rect(x,height,this.width,this.height * -5);
-  }
-  
-  this.renderCircleTop = function(x) {
-    var circleWidth = this.width * 2;
-    fill("hotpink");
-    ellipse((x*10)+(this.width / 2), this.dramaticHeight, circleWidth, circleWidth);
-  }
-  
-  this.render = function(i) {
-    this.renderRect(i);
-    this.renderCircleTop(i);
-  }
-}
-
+var dataArr = [];
+var dataArrLength = dataArr.length;
+var dataValueText = "Hover over match stick head to see values";
 
 function setup() {
   for(var i = 0; i < dataLength; i++) {
@@ -43,4 +22,65 @@ function draw() {
   fill("#fefefe");
   //textFont("NAME OF FONT") add url into html
   text("Intro Text", 20, 1000);
+}
+
+
+var Adata = function(localHeight) {
+  this.height = localHeight;
+  this.width = 10;
+  this.dramaticHeight = this.height * -5;
+
+  var circleWidth = this.width * 2;
+
+  this.renderRect = function(x) {
+    fill("#bada55");
+    rect(x,height,this.width,this.dramaticHeight);
+  };
+
+  this.renderCircleTop = function(x) {
+    fill("hotpink");
+    ellipse((x) + (this.width / 2), height + this.dramaticHeight, circleWidth, circleWidth);
+  };
+
+  this.isMouseCloseToCircle = function(posnX, posnY, x) {
+    if (dist(posnX, posnY, x, height + this.dramaticHeight) < circleWidth) {
+      dataValueText = this.height;
+    }
+  }
+
+  this.render = function(iterator) {
+    var x = iterator * 10;
+    this.renderRect(x);
+    this.renderCircleTop(x);
+    this.isMouseCloseToCircle(mouseX, mouseY, x);
+  }
+}
+
+function setup() {
+  for (var i = 0; i < dataLength; i++) {
+    dataArr.push(new Adata(data[i]));
+  }
+  createCanvas(500,500);
+  dataArrLength = dataArr.length;
+}
+
+function draw() {
+  background('#333');
+  for (var i = 0; i < dataArrLength; i++){
+    dataArr[i].render(i);
+  }
+
+  fill("#fefefe");
+  textSize(22);
+  // textFont("Bangers")
+  text(dataValueText, 20, 100);
+
+hit=collidePointRect(mouseX,mouseY,x,height,this.width,this.dramaticHeight);
+
+if(hit){
+  fill("#fefefe");
+  textSize(22);
+  text(dataValueText,20,100);
+}
+
 }
